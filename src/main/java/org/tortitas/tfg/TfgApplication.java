4dc6c9611bc96a08312e9ccf6352cbbda6e0a2cb
StarlightAura <auracv.vs@gmail.com>
2026-03-12 18:43:03 +0100

    mongodb

--- src/main/java/org/tortitas/tfg/TfgApplication.java
+++ src/main/java/org/tortitas/tfg/TfgApplication.java
@@ -1,14 +1,45 @@
 package org.tortitas.tfg;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.tortitas.tfg.services.OllamaService;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.tortitas.tfg.models.Game;
+import org.tortitas.tfg.repositories.GameRepository;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.List;
 
 @SpringBootApplication
-public class TfgApplication {
+@EnableMongoRepositories
+public class TfgApplication implements CommandLineRunner {
+
+    @Autowired
+    GameRepository gameRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(TfgApplication.class, args);
+    }
+
+    public void run(String... args){
+
+        try{
+            createGames();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void createGames() throws FileNotFoundException {
+        Gson gson = new Gson();
+        List<Game> games = gson.fromJson(new FileReader("/home/aura/IDEAProjects/tortitasConSirope/src/main/resources/out.json"), new TypeToken<List<Game>>() {});
+        gameRepository.saveAll(games);
     }
 
 }
--- src/main/java/org/tortitas/tfg/models/Game.java
+++ src/main/java/org/tortitas/tfg/models/Game.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 
+@Document("GameItem")
 public class Game {
+    @Id
     public int sid;
     public String store_url;
     public int store_uscore;
@@ -19,7 +23,7 @@ public class Game {
     public String description;
     public int full_price;
     public String developers;
-    public String languanges;
+    public String languages;
     public String genres;
     public String tags;
 }
