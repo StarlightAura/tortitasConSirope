b8790c7c7e7c1eab52ce4429f46a04b4b51cce56
iris-cafe <laura.mrtmrtz@gmail.com>
2026-03-22 15:23:54 +0100

    usar format en vez de toString

--- src/main/java/org/tortitas/tfg/models/Game.java
+++ src/main/java/org/tortitas/tfg/models/Game.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Vector;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 
+@Document("GameItem")
 public class Game {
+    @Id
     public int sid;
     public String store_url;
     public int store_uscore;
@@ -19,7 +25,28 @@ public class Game {
     public String description;
     public int full_price;
     public String developers;
-    public String languanges;
+    public String languages;
     public String genres;
     public String tags;
+
+    public Vector<Double> embeddings; //Para guardar los vectores que genera nuestra amiga Ollama
+
+    // Si lo pongo sin el org me chilla por el document de mongo
+    public org.springframework.ai.document.Document game2document(){
+
+        /*Si le pasamos tooodo ollama se puede volver crazy porque necesita contexto
+        para crear los vectores o embeddings o como se diga, y resulta y acontece que por lo que sea
+        saber el precio del jueguito le importa entre poco y nada por ejemplo
+        */
+        String juegosContent = String.format(
+                /*Por alguna razón, la ia en general, no entiende el contexto de algunas cosas si no
+                * vas con ella de la manita, me explico, el nombre y la descripcion lo entiende perfe,
+                * sin embargo, si no le dices explicitamente con etiquetas que son el resto de cosas
+                * se vuelve tonta y puede pensar que el genero terror es el nombre de el desarrollador por ejemplo
+                * y en resumen liarla parda*/
+                "%s. %s. Genres: %s. Tags: %s. Developers: %s",
+                this.name, this.description, this.genres, this.tags, this.developers
+        );
+        return new org.springframework.ai.document.Document(juegosContent);
+    }
 }
