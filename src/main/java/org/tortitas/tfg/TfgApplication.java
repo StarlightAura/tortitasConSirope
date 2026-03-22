402ed7cb62564f86a81f5e3d4d5f367315d1d879
iris-cafe <laura.mrtmrtz@gmail.com>
2026-03-22 18:28:37 +0100

    Base para telegram

--- src/main/java/org/tortitas/tfg/TfgApplication.java
+++ src/main/java/org/tortitas/tfg/TfgApplication.java
@@ -2,23 +2,16 @@ package org.tortitas.tfg;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.mongodb.MongoException;
-import com.mongodb.client.result.InsertOneResult;
-import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.tortitas.tfg.controllers.EmbeddingController;
 import org.tortitas.tfg.models.Game;
-import org.tortitas.tfg.repositories.GameRepository;
-import org.tortitas.tfg.services.OllamaService;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -49,31 +42,44 @@ public class TfgApplication implements CommandLineRunner {
 
     }
 
-
-    //PROBLEMITA, ESTO NO NOS SIRVE PARA LA CANTIDAD INGENTE DE JUEGOS QUE TENEMOS
-   public void createGames() throws FileNotFoundException {
+    public void createGames() throws FileNotFoundException {
         Gson gson = new Gson();
-        List<Game> games = gson.fromJson(
-                new FileReader("out.json"),
-                new TypeToken<List<Game>>() {}
-        );
+        List<Game> games = gson.fromJson(new FileReader("/home/aura/IDEAProjects/tortitasConSirope/src/main/resources/out.json"), new TypeToken<List<Game>>() {});
+
+
 
         for (Game game : games) {
-            /*Aqui es donde se volveria un poco crazy si le pasamos lo de antes
-            * por eso el format para llevar de la manita a ollama creando un texto que entienda
-            * y asi devuelva los vectores para que todos estemos contentos*/
-            float[] raw = embeddingController.embed(game.game2document());
+            float[] embeddings = embeddingController.embed(game.game2document());
+            //System.out.println(Arrays.toString(embeddings));
 
-            //Just un bucle que traduce los floats de 32b de ollama a los 64b que usamos
-            Vector<Double> embeddingVector = new Vector<>();
-            for (float f : raw) {
-                embeddingVector.add((double) f);
+            double[] dEmbeddings = IntStream.range(0, embeddings.length).mapToDouble(i -> embeddings[i]).toArray();
+            List<Double> d2Embeddings = Arrays.stream(dEmbeddings).boxed().toList();
+
+            Vector<Double> vEmbeddings = new Vector<Double>(d2Embeddings);
+            game.setEmbeddings(vEmbeddings);
+
+
+
+            //TODO: fix the document mess
+            /*
+            String insertedId;
+
+            try{
+                InsertOneResult result = collection.insertOne(document);
+                System.out.println("Inserted id: " + result.getInsertedId());
+            } catch (MongoException me){
+                throw new RuntimeException("Error inserting games", me);
             }
+            */
 
-            game.embeddings = embeddingVector; //asigna vectores a nuestro objetito
+            System.out.println(game);
+
             gameRepository.save(game);
-            System.out.println("Se ha guardado el jueguito -> " + game.name);
+
         }
+
+        //gameRepository.saveAll(games);
+
     }
 
 }
