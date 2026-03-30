5e88a729fcb63f876efa3f419c2123c3a229d4c5
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-03-30 22:59:24 +0200

    Agregando archivos de configuración

---
+++ src/main/java/org/tortitas/tfg/mysql/login/config/JwtConfig.java
@@ -0,0 +1,24 @@
+package org.tortitas.tfg.mysql.login.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+
+@Getter
+@AllArgsConstructor
+
+@ConfigurationProperties(prefix = "application.security.jwt")
+public class JwtConfig {
+
+
+    private final String secret;
+
+    private final long expiration;
+
+    private final long refreshTokenExpiration;
+
+
+}
