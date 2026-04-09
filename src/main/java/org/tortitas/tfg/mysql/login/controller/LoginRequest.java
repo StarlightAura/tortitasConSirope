cc617993ad85eec6448fa4bfa0d10201e0230075
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-09 18:25:10 +0200

    Añadiendo validaciones en el LoginRequest

--- src/main/java/org/tortitas/tfg/mysql/login/controller/LoginRequest.java
+++ src/main/java/org/tortitas/tfg/mysql/login/controller/LoginRequest.java
@@ -1,7 +1,16 @@
 package org.tortitas.tfg.mysql.login.controller;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.tortitas.tfg.mysql.login.validation.ValidPassword;
+
 public record LoginRequest(
+        @NotBlank
+        @Email(message = "Formato de email incorrecto.")
         String email,
+
+        @NotBlank
+        @ValidPassword
         String password
 ) {
 }
