237cb5154e4a7c4b8b01abce1f1f51ba72a0ac1f
iris-cafe <laura.mrtmrtz@gmail.com>
2026-04-10 19:11:28 +0200

    He borrado codigo innecesario

--- src/main/java/org/tortitas/tfg/models/JWTToken.java
+++ src/main/java/org/tortitas/tfg/models/JWTToken.java
@@ -10,54 +10,6 @@ import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 import org.jose4j.lang.JoseException;
 import org.springframework.stereotype.Component;
 
-//public class JWTToken {
-//
-//    public static void jwtToken() throws JoseException {
-//
-//        RsaJsonWebKey rsaJsonWebKey = RsaJwkGenerator.generateJwk(2048);
-//        rsaJsonWebKey.setKeyId("k1"); // key id
-//        JwtClaims claims = new JwtClaims(); // contenido del JWT
-//
-//        claims.setIssuer("Tortitas"); // creador y firmador del token
-//        claims.setAudience("Audiencia Nacional"); // a quien se envia el token
-//        claims.setExpirationTimeMinutesInTheFuture(10); // tiempo de vida del token en minutos
-//        claims.setGeneratedJwtId(); // identificador unico del token
-//        claims.setIssuedAtToNow(); // cuando se crea el token (ahora mismo)
-//        claims.setNotBeforeMinutesInThePast(2); // tiempo anterior al que el token no es válido (2 minutos atrás)
-//        claims.setSubject("Sujeto");
-//        claims.setClaim("email", "tortitas@sirope.com");
-//
-//        // un JWT es un JWS (JSON Web Signature) con JSON como payload
-//        JsonWebSignature jws = new JsonWebSignature();
-//
-//        jws.setPayload(claims.toJson()); // el payload del JWS es el contenido JSON de JWT Claims
-//        jws.setKey(rsaJsonWebKey.getPrivateKey()); // se firma con la clave privada
-//        jws.setKeyIdHeaderValue(rsaJsonWebKey.getKeyId()); // se pone la keyID
-//        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256); // algoritmo de firma para proteger la integridad de JWT Claims
-//
-//        // firma el JWS y produce serialización
-//        // con el formato
-//        // Header.Payload.Firma codificado en base64url
-//        // si se quiere encriptar se pone este JWT como payload de un objeto JsonWebEncryption
-//        // y se pone el cty (Content Type) a "jwt"
-//        String jwt = jws.getCompactSerialization();
-//
-//        System.out.println(jwt);
-//
-//    }
-//
-//    public static void consumeJWT() throws JoseException {
-///*
-//        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
-//                .setRequireExpirationTime() // tiene que tener tiempo de vida
-//                .setAllowedClockSkewInSeconds(30)
-//
-// */
-//    }
-//}
-
-import org.springframework.stereotype.Component;
-
 @Component
 public class JWTToken {
 
@@ -69,7 +21,7 @@ public class JWTToken {
         this.rsaJsonWebKey.setKeyId("k1");
     }
 
-    public String generateToken(String username) throws JoseException {
+    public String generateToken(String username, Rol rol) throws JoseException {
         JwtClaims claims = new JwtClaims();
         claims.setIssuer("Tortitas");
         claims.setAudience("Audiencia Nacional");
@@ -78,7 +30,8 @@ public class JWTToken {
         claims.setIssuedAtToNow();
         claims.setNotBeforeMinutesInThePast(2);
         claims.setSubject(username);
-        claims.setClaim("email", username);
+        claims.setClaim("rol", rol.name()); //para guardar el rol de usuario en el token
+       // claims.setClaim("email", username); ¿esto pa que lo teniamos?
 
         JsonWebSignature jws = new JsonWebSignature();
         jws.setPayload(claims.toJson());
@@ -88,6 +41,43 @@ public class JWTToken {
 
         return jws.getCompactSerialization();
     }
+
+    //Nuevito -> para sacar el rol sin hacer toda la validacion again
+    public String getRolFromToken(String token) throws Exception {
+        JwtClaims claims = getClaims(token);
+        return claims.getStringClaimValue("rol"); //la idea es que segun el rol, muestre lo de insertar juego o no en el controlador
+    }
+
+
+    // metodo privado aux para no repetir el codigo de validacion
+    // aqui se explica mejor -> https://www.arquitecturajava.com/el-principio-dry-y-la-encapsulacion/
+    private JwtClaims getClaims(String token) throws Exception {
+        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+                .setRequireExpirationTime()
+                .setAllowedClockSkewInSeconds(30)
+                .setRequireSubject()
+                .setExpectedIssuer("Tortitas")
+                .setExpectedAudience("Audiencia Nacional")
+                .setVerificationKey(rsaJsonWebKey.getPublicKey())
+                .build();
+
+        return jwtConsumer.processToClaims(token);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            //aqui solo he cambiado que en vez de ser  validateTokenAndGetUsername(token) sea el nuevo
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /*
+
+    URL DEL SITIO POR SI QUEREIS REVISAR: https://www.descope.com/learn/post/jwt-claims
+    EXTRA QUIZA INTERESANTE: https://www.baeldung.com/spring-security-thymeleaf
 
     public String validateTokenAndGetUsername(String token) throws Exception {
         JwtConsumer jwtConsumer = new JwtConsumerBuilder()
@@ -109,5 +99,5 @@ public class JWTToken {
         } catch (Exception e) {
             return false;
         }
-    }
+    }*/
 }
