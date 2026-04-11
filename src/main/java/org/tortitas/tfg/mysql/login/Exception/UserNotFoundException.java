9516fb3422e5a9fbafc8c97b113b0600bb2679a5
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-11 17:45:30 +0200

    Borrando los archivos para el login con MySQL

--- src/main/java/org/tortitas/tfg/mysql/login/Exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.tortitas.tfg.mysql.login.Exception;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String message){
-       super(message);
-    }
-
-}
--- src/main/java/org/tortitas/tfg/mysql/login/model/Role.java
+++
@@ -1,7 +0,0 @@
-package org.tortitas.tfg.mysql.login.model;
-
-public enum Role {
-
-    ADMIN, USER
-
-}
