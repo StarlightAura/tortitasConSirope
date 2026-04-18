23cb81902efb67df64247035d570bfc7a76c8881
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-18 19:43:50 +0200

    Cambiando el nombre del regex

--- src/main/java/org/tortitas/tfg/dto/CreateUserDTO.java
+++ src/main/java/org/tortitas/tfg/dto/CreateUserDTO.java
@@ -1,23 +1,19 @@
 package org.tortitas.tfg.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public class CreateUserDTO {
     private final String name;
     private final String password;
-    private final String regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$";
+    private final String REGEXP = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$";
 
     public CreateUserDTO(String name, String password){
         if (name.length()<2){
             throw new IllegalArgumentException("Nombre de usuario demasiado corto");
         }
 
-        if (!password.matches(regexp)){
+        if (!password.matches(REGEXP)){
             throw new IllegalArgumentException(
                     "La contraseña debe tener almenos 8 caracteres formada por" +
                     " mayúsculas, minúsculas, número y símbolo"
